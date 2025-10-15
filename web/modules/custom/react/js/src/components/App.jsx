d19b9fb56d3bdcd84737a233b4ff4d09e30dbc8d
Nolan Chappuis <nolan.chappuis@tcs.ch>
2025-10-15 13:36:17 +0200

    import react into drupal

---
+++ web/modules/custom/react/js/src/components/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const App = () => {
+  const [count, setCount] = React.useState(0);
+
+  return (
+    <div>
+      <h1>Hello from React Component!</h1>
+      <p>Counter: {count}</p>
+      <button onClick={() => setCount(count + 1)}>
+        Click me
+      </button>
+    </div>
+  );
+};
+
+export default App;
---
+++ web/modules/custom/react/src/Plugin/Block/ReactBlock.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Drupal\react\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+
+
+/**
+ * Provides a 'ReactBlock' block.
+ * 
+ * @Block(
+ *   id = "react_block",
+ *   admin_label = @Translation("React block")
+ * )
+*/
+class ReactBlock extends BlockBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    $build = [];
+    $build['react_block'] = [
+      '#markup' => '<div id="react-app"></div>',
+      '#attached' => [
+        'library' => ['react/react_app']
+      ],
+    ];
+    return $build;
+  }
+}
