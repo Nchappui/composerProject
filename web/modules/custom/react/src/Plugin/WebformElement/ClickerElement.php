2540916bb959c33632db96a3e9d78e1836900e37
Nolan Chappuis <nolan.chappuis@tcs.ch>
2025-10-21 14:23:42 +0200

    Refactor ClickerElement and SliderElement for improved property handling and cleanup

--- web/modules/custom/react/src/Plugin/WebformElement/ClickerElement.php
+++ web/modules/custom/react/src/Plugin/WebformElement/ClickerElement.php
@@ -2,13 +2,10 @@
 
 namespace Drupal\react\Plugin\WebformElement;
 
-use Drupal\Core\Form\FormStateInterface;
 use Drupal\webform\Plugin\WebformElementBase;
 use Drupal\webform\WebformSubmissionInterface;
 
 /**
- * Provides a 'clicker' element.
- *
  * @WebformElement(
  *   id = "react_clicker",
  *   label = @Translation("React Clicker"),
@@ -20,64 +17,49 @@ use Drupal\webform\WebformSubmissionInterface;
  */
 class ClickerElement extends WebformElementBase {
 
-  /**
-   * {@inheritdoc}
-   */
   public function getDefaultProperties() {
-    return [
-      // Element settings.
-      'multiple' => FALSE,
-      'title' => '',
-      'default_value' => '0',
-    ] + parent::getDefaultProperties();
+    return ['default_value' => '0'] + parent::getDefaultProperties();
   }
 
-  /**
-   * {@inheritdoc}
-   */
   public function prepare(array &$element, WebformSubmissionInterface $webform_submission = NULL) {
     parent::prepare($element, $webform_submission);
 
-    // Set the element type to hidden (it will store the value)
     $element['#type'] = 'hidden';
     $element['#attributes']['class'][] = 'react-clicker-value';
-    
-    // Get stored value or default to 0
-    $stored_value = $element['#default_value'] ?? '0';
-    $element['#default_value'] = $stored_value;
-    $element['#attributes']['data-initial-value'] = $stored_value;
-    
-    // Add library
+    $element['#attributes']['data-initial-value'] = $element['#default_value'] ?? '0';
     $element['#attached']['library'][] = 'react/react_clicker_app';
-    
-    // Add wrapper that will contain both the React app and the hidden input
     $element['#prefix'] = '<div class="react-clicker-wrapper"><div class="react-clicker-app"></div>';
     $element['#suffix'] = '</div>';
   }
+  
+  // getValue(), formatHtmlItem(), formatTextItem() peuvent être supprimées
+  // La classe parente les gère déjà correctement
 
-  /**
-   * {@inheritdoc}
-   */
-  public function getValue(array $element, WebformSubmissionInterface $webform_submission, array $options = []) {
-    $value = $webform_submission->getElementData($element['#webform_key']);
-    return $value ?? '0';
-  }
 
-  /**
-   * {@inheritdoc}
-   */
-  public function formatHtmlItem(array $element, WebformSubmissionInterface $webform_submission, array $options = []) {
-    $value = $this->getValue($element, $webform_submission, $options);
-    return [
-      '#markup' => (string) $value,
-    ];
-  }
+//   /**
+//    * {@inheritdoc}
+//    */
+//   public function getValue(array $element, WebformSubmissionInterface $webform_submission, array $options = []) {
+//     $value = $webform_submission->getElementData($element['#webform_key']);
+//     return $value ?? '0';
+//   }
 
-  /**
-   * {@inheritdoc}
-   */
-  protected function formatTextItem(array $element, WebformSubmissionInterface $webform_submission, array $options = []) {
-    $value = $this->getValue($element, $webform_submission, $options);
-    return (string) $value;
-  }
+//   /**
+//    * {@inheritdoc}
+//    */
+//   public function formatHtmlItem(array $element, WebformSubmissionInterface $webform_submission, array $options = []) {
+//     $value = $this->getValue($element, $webform_submission, $options);
+//     return [
+//       '#markup' => (string) $value,
+//     ];
+//   }
+
+//   /**
+//    * {@inheritdoc}
+//    */
+//   protected function formatTextItem(array $element, WebformSubmissionInterface $webform_submission, array $options = []) {
+//     $value = $this->getValue($element, $webform_submission, $options);
+//     return (string) $value;
+//   }
+   
 }
